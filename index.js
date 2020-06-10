9fe7a0ef4bf9bdbb5d9b334d3b0a278464b61933
lchirata <larissa.c.hirata@gmail.com>
2020-06-10 00:20:04 -0300

    criar pedido produto

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const PORT = 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const moment = require('moment');
+
 app.use(cors());
 
 require('./database/index');
 
 const Produto = require('./models/Produto');
 const Cliente = require('./models/Cliente');
+const Pedido = require('./models/Pedido');
+const PedidoProduto = require('./models/PedidoProduto');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -70,5 +74,49 @@ app.post('/clientes', async (request, response) =>{
     });
     response.json(cliente);
 });
+
+app.post('/pedidos', async (request, response)=>{
+    let total =0;
+    const data = moment().format('DD/MM/YYYY');
+
+    const body = request.body;
+    const id_cliente = body.id_cliente;
+    const produtos = body.produtos; //ARRAY DE PRODUTOS DA REQUEST 
+    const prods = [];
+
+    //TOTAL DO PEDIDO
+    for(const produto of produtos){
+        console.log(produto);
+        const prod = await Produto.findOne({
+            where: {
+                id: produto.id_produto,
+            }
+        });
+
+        prod.quantidade = produto.quantidade;
+        prods.push(prod);
+        total = total + (prod.preco * produto.quantidade);
+    }
+
+    //CRIA PEDIDO
+
+    const pedido = await Pedido.create({
+        cliente_id: id_cliente,
+        total: total, 
+        data: data, 
+    });
+
+    //RELACIONAMENTO PEDIDO-PRODUTO
+    for (const prod of prods) {
+        await PedidoProduto.create({
+            id_pedido: pedido.id, 
+            id_produto: prod.id,
+            preco: prod.preco,
+            quantidade: prod.quantidade
+        });
+    }
+
+    response.json(pedido);
+})
 
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
--- models/Pedido.js
+++ models/Pedido.js
@@ -4,6 +4,7 @@ const DataTypes = require('sequelize').DataTypes;
 class Pedido extends Model {
     static init(connection) {
         super.init({
+            cliente_id: DataTypes.INTEGER,
             total: DataTypes.FLOAT,
             data: DataTypes.STRING,
         }, {
